25b47c8e7dfd228bdfd10a30a4f975abbb87e162
skyblued <806020149@qq.com>
2018-09-09 20:34:57 +0800

    dongtai

---
+++ aliyun/js/header.js
@@ -0,0 +1,46 @@
+
+       
+
+
+
+// hb 悬停事件
+var hbbao = document.querySelector(".header-left-hb")
+var headerHover = document.querySelector(".header-hover");
+var hhLeft = document.querySelector(".header-hover-left");
+var hhCenter = document.querySelector(".header-hover-center");
+var hhRight = document.querySelector(".header-hover-right");
+var body = document.body;
+hbbao.onmouseover = function(){
+    hhLeft.style.width = `300px`;
+    if(getComputedStyle(hhLeft).width != 0){
+        hbbao.style.background = `#00c1de`
+    }
+}
+hhLeft.onmouseover = function(e){
+    if(e.target.nodeName == "LI")
+    hhCenter.style.width = `300px`;
+}
+body.onmouseout = function(e){
+    if( (e.clientX>1100 || e.clientY<50) ){
+        hhCenter.style.width = 0;
+        hhLeft.style.width = 0;
+        hhRight.style.width = 0;
+        hbbao.style.background = `none`;
+    } 
+    if(e.clientX<600){
+        hhRight.style.width = 0;
+    }
+}
+hhCenter.onmouseover = function(e){
+    if(e.target.nodeName == "LI" || e.target.nodeName == "SPAN")
+    hhRight.style.width = `500px`;
+}
+hhCenter.onmouseout = function(){
+    hhRight.style.width = 0;
+}
+hhRight.onmouseover = function(){
+    hhRight.style.width = `500px`;
+}
+// hhRight.onmouseout = function(){
+//     hhRight.style.width = 0;
+// }
--- aliyun/js/index.js
+++ aliyun/js/index.js
@@ -7,12 +7,12 @@ var btn = document.getElementsByClassName("btn")[0];
 var n = 0;
      setInterval( function(){
         var banner = document.getElementsByClassName("banner-item")[0];
-            n+=640;
-        if(n>640*3){
+            n+=700;
+        if(n>700*3){
             n = 0;
             banner.style.transition = `0s`;
-        }else if(n<640*3){
-            banner.style.transition = `0.2s`;
+        }else{
+            banner.style.transition = `0.5s`;
         }
         banner.style.marginTop = `-${n}px`;
     },3000)
